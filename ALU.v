c8b17a64b6d77a0afcfaf9f02b2330310b284bd0
Camille Silva Oliveira <cmille2004@gmail.com>
2025-08-24 23:57:21 -0300

    Changing a couple of informations

--- ALU.v
+++ ALU.v
@@ -1,30 +1,29 @@
 
-module ALU(
-    input  [31:0] A,         
-    input  [31:0] B,         
-    input  [3:0]  ALUControl, 
-    output [31:0] Result,     
-    output        Zero        
+module ULA(
+    input  [31:0] EntradaA,
+    input  [31:0] EntradaB,
+    input  [3:0]  ControleULA,
+    output [31:0] Resultado,
+    output        Zero
 );
 
-    reg [31:0] alu_result;
+    reg [31:0] resultado_ula;
 
     always @(*) begin
-        case (ALUControl)
-            4'b0000: alu_result = A & B;        
-            4'b0001: alu_result = A | B;         
-            4'b0010: alu_result = A + B;         
-            4'b0110: alu_result = A - B;         
-            4'b0111: alu_result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0; 
-            4'b1100: alu_result = A ^ B;         
-            4'b1000: alu_result = A << B[4:0];   
-            4'b1001: alu_result = A >> B[4:0];   
-            default: alu_result = 32'hxxxxxxxx; 
+        case (ControleULA)
+            4'b0000: resultado_ula = EntradaA & EntradaB;
+            4'b0001: resultado_ula = EntradaA | EntradaB;
+            4'b0010: resultado_ula = EntradaA + EntradaB;
+            4'b0110: resultado_ula = EntradaA - EntradaB;
+            4'b0111: resultado_ula = ($signed(EntradaA) < $signed(EntradaB)) ? 32'd1 : 32'd0;
+            4'b1100: resultado_ula = EntradaA ^ EntradaB;
+            4'b1000: resultado_ula = EntradaA << EntradaB[4:0];
+            4'b1001: resultado_ula = EntradaA >> EntradaB[4:0];
+            default: resultado_ula = 32'hxxxxxxxx;
         endcase
     end
 
-    assign Result = alu_result;
-    
-    assign Zero = (alu_result == 32'b0);
+    assign Resultado = resultado_ula;
+    assign Zero = (resultado_ula == 32'b0);
 
 endmodule
